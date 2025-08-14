9d5f61abda4cb629d7a6f0ff12ded01b801c3cfa
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-08-13 22:07:51 -0500

    add placeholder for the user site

---
+++ src/app/(main_site)/home/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Home page",
+};
+
+export default function Home() {
+  return <div>Home</div>;
+}
